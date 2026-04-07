2a0be9c9b56e1b5daf0803d3353fe58a23abd4ca
giselefbrito-ux <gisele.fbrito@upe.br>
2026-04-07 15:19:46 -0300

    Update binary_search.py

--- src/binary_search.py
+++ src/binary_search.py
@@ -7,4 +7,17 @@ def binary_search(array: MyArray, target: int) -> int:
 
     Deve retornar o índice do elemento ou -1 caso não encontrado.
     """
-    raise NotImplementedError
+    esquerda_array = 0
+    direita_array = -1
+
+    while esquerda_array <= direita array:
+        meio =  esquerda_array+direita_array // 2
+        value = array.get(mid)
+
+        if value == target:
+            return meio
+        elif value < target:
+            esquerda_array = meio + 1
+        else:
+             direita_array = meio - 1
+            
